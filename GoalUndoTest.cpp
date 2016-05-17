8baae29150cb4782faf26675a594962924de626d
Abhinav Gupta <abhi.951990@gmail.com>
2016-05-17 01:41:58 -0700

    Checked addition of goals

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,18 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+
+TEST(GoalUndoTest, check_addition_of_goals){
+  GoalUndo g;
+  g.addOperation("Reach target","Complete Task 1");
+  ASSERT_TRUE(g.getGoal()=="Reach target");
+}
+
+TEST(GoalUndoTest,check_addition_of_operations){
+  GoalUndo g;
+  g.addOperation("Reach target","Complete Task 1");
+  g.addOperation("Complete Task 2");
+  ASSERT_TRUE(g.getOperations()=="Complete Task 1 Complete Task 2");
 }
